bbd43c99f4a783cc1209573069314c6d4681acf3
Julien <julien@dokit.io>
2018-11-29 14:27:52 +0100

    [PageViewsProperty] trop de jobs lancés

--- PageViewsProperty.hooks.php
+++ PageViewsProperty.hooks.php
@@ -4,6 +4,7 @@ namespace PageViewsProperty;
 use OutputPage;
 use Skin;
 use JobQueueGroup;
+use SMWDINumber;
 
 class Hooks {
 
@@ -25,14 +26,90 @@ class Hooks {
 
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 
-		$jobParams = array();
+		/*global $wgScriptPath, $wgArticlePath, $wgScript, $IP, $wgUploadDirectory, $wgServer, $wgUploadPath, $wgExtensionAssetsPath;
 
-		$title = $skin->getTitle();
+		var_dump($wgScriptPath);
+		var_dump($wgArticlePath);
+		var_dump($wgScript);
+		var_dump($IP);
+		var_dump($wgUploadDirectory);
+		var_dump($wgServer);
+		var_dump($wgUploadPath);
+		var_dump($wgExtensionAssetsPath);*/
 
-		if($title){
-			$job = new PageViewsPropertyUpdateJob( $title, $jobParams );
-			JobQueueGroup::singleton()->push( $job ); // mediawiki >= 1.21
+		$lastUpdated = self::getPagePropLastUpdated($out);
+		if (is_null($lastUpdated) || ($lastUpdated && self::needsRefresh($lastUpdated)) ) {
+
+			$jobParams = array();
+
+			$title = $skin->getTitle();
+
+			if($title){
+				$job = new PageViewsPropertyUpdateJob( $title, $jobParams );
+				JobQueueGroup::singleton()->push( $job ); // mediawiki >= 1.21
+			}
+
+			self::setPagePropLastUpdated($out);
 		}
+	}
+
+
+	public static function setLastUpdated($output) {
+
+		$wgMemc = wfGetMainCache();
+
+		$key = $wgMemc->makeKey('views', 'property', 'lastupdated');
+
+		$date = new \DateTime();
+
+		if($wgMemc->get($key)){
+			$wgMemc->set($key, $date->format('Y-m-d H:i:s'));
+		}else{
+			$wgMemc->add($key, $date->format('Y-m-d H:i:s'));
+		}
+	}
+
+	public static function getLastUpdated($output) {
+
+		$wgMemc = wfGetMainCache();
+
+		$key = $wgMemc->makeKey('views', 'property', 'lastupdated');
+
+		if($res = $wgMemc->get($key)){
+			return $res;
+		}else{
+			return null;
+		}
+	}
+
+	public static function needsRefresh( $lastUpdated ) {
+
+		global $wgPageViewsPropertyRefreshDelayInterval;
+
+		$needsRefresh = true;
+
+		$lastUpdated = new \DateTime( $lastUpdated );
+		$now = new \DateTime();
+
+		if ($wgPageViewsPropertyRefreshDelayInterval) {
+
+			try {
+			    $refreshDelayInterval = new \DateInterval($wgPageViewsPropertyRefreshDelayInterval);
+
+			    if ($lastUpdated->add($refreshDelayInterval) < $now) {
+					$needsRefresh = true;
+				} else {
+					$needsRefresh = false;
+				}
+			} catch (Exception $e) {
+			    echo "L'intervalle spécifié dans la variable globale ne correspond pas à un intervalle correct. Pour plus d'informations, http://www.lephpfacile.com/manuel-php/dateinterval.construct.php . \n";
+			}
+
+		} else {
+			$needsRefresh = true;
+		}
+
+		return $needsRefresh;
 	}
 
 
